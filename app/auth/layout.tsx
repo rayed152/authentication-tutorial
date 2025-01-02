feb42bfea9e87cd1d40f414d494de328a369a380
rayed152 <rayed152@gmail.com>
2025-01-02 12:41:44 +0600

    done making the UI of login and also made some error and success message components. Use zod for validation aswell

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -2,7 +2,12 @@ import React from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full flex items-center justify-center">{children}</div>
+    <div
+      className="h-full flex items-center justify-center"
+      suppressHydrationWarning
+    >
+      {children}
+    </div>
   );
 };
 
